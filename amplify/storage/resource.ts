a8b90edb48257461f08a8cf8231c309618f635be
Ava Muto <mutoa@student.mville.edu>
2025-11-19 07:17:30 -0500

    fix

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,7 +2,7 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'collectionPhotos',
-  access: (allow) => ({
+  access: allow => ({
     'pictures/*': [
       allow.authenticated.to(['read', 'write', 'delete'])
     ],
