d49e8998178e3e7357190260a23aa6c119261f04
Ava Muto <mutoa@student.mville.edu>
2025-12-09 11:57:36 -0500

    deleted extra

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,28 +1,4 @@
 import { defineAuth } from '@aws-amplify/backend';
-import { a, defineData } from "@aws-amplify/backend";
-
-const schema = a.schema({
-  User: a.model({
-    id: a.id(),
-    username: a.string(),
-    bio: a.string(),
-    avatarUrl: a.string(),
-    collections: a.hasMany('Collection', 'owner')
-  }),
-
-  Collection: a.model({
-    id: a.id(),
-    title: a.string(),
-    description: a.string(),
-    imageUrl: a.string(),
-    owner: a.belongsTo('User', 'collections'),
-    createdAt: a.datetime()
-  })
-});
-
-export const data = defineData({
-  schema,
-});
 
 export const auth = defineAuth({
   loginWith: {
